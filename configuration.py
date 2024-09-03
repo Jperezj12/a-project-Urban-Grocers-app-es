cc9e84efa5c1d4d7a05526f8a6d7e0fb7f047024
Jperez12 <jlp_perez84@yahoo.com.mx>
2024-09-03 08:32:43 -0600

    Mi proyecto final Sprint7 V.4

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-1e2a9a3f-bf83-43b2-82af-24adcfd0edaf.containerhub.tripleten-services.com/"
+URL_SERVICE = "https://cnt-d0388e18-9331-47f2-9c94-f193cc6f378d.containerhub.tripleten-services.com/"
 CREATE_USER_PATH = "/api/v1/users/"
 CREATE_KIT = "/api/v1/kits"
 
--- sender_request.py
+++ sender_request.py
@@ -9,18 +9,20 @@ def post_new_user(body):
                          headers=data.headers)
 
 
-# Hacer la solicitud para crear el usuario
-user_response = post_new_user(data.user_body)
-response_json = user_response.json()
-# Guardar el authToken en una variable
-auth_token = response_json.get("authToken")
-
-
 def post_kit(new_kit):
+    # Hacer la solicitud para crear el usuario
+    user_response = post_new_user(data.user_body)
+    response_json = user_response.json()
+    # Guardar el authToken en una variable
+    auth_token = response_json.get("authToken")
+
+    # Concatenar el token de autorización en el encabezado
+    autorizacion = {
+        "Content-Type": "application/json",
+        "Authorization": f'Bearer {auth_token}'
+    }
+
+    # Hacer la solicitud para crear el kit
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_KIT,
                          json=new_kit,
                          headers=autorizacion)
-autorizacion = {
-    "Content-Type": "application/json",
-    "Authorization": f'Bearer {auth_token}'
-}
